b5b67fe9c3da48348f000089c420668ae74529fb
Mithil Pawar <mithilpawar7044@gmai.com>
2026-01-30 15:04:40 +0530

    DB constant added

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,10 +1,11 @@
 import mysql from "mysql2/promise";
+import { DB_NAME } from "../constants/db.constants.js";
 
 const pool = mysql.createPool({
   host: "localhost",
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: "nimap_test",
+  database: DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
